cbe2b48dc8863e9a1c629148e703edb6492a9c6d
carlosceriani <carlos.ceriani@gmail.com>
2021-10-28 18:30:32 -0300

    First commit

---
+++ Classes/StringHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace LongestSubstring.Classes
+{
+    public class StringHelper
+    {
+        public StringHelper()
+        {
+        }
+
+        public int GetLongestSequenceLength(string word)
+        {
+            if (word == null)
+            {
+                return 0;
+            }
+
+            var longest = 0;
+            var start = 0;
+            string current;
+            int position;
+            int i;
+
+            while (start < word.Length)
+            {
+                current = "";
+                for (i = start; i < word.Length; i++)
+                {
+                    position = current.IndexOf(word[i]);
+                    if (position < 0)
+                    {
+                        current += word[i];
+                    }
+                    else
+                    {
+                        if (current.Length > longest)
+                        {
+                            longest = current.Length;
+                        }
+                        start += position + 1;
+                        break;
+                    }
+                }
+                if (i == word.Length)
+                {
+                    if (current.Length > longest)
+                    {
+                        longest = current.Length;
+                    }
+                    break;
+                }
+            }
+
+
+            return longest;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LongestSubstring.Classes;
+
+namespace LongestSubstring
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            /* TASK: Given a string, find the length of the longest substring without repeating characters.
+             * 
+             * Test cases:
+             * Input "abcabcdbb"      - Expected output: 4 ("abcd")
+             * Input "pwwkew"         - Expected output: 3 ("wke")
+             * Input "abcabcdbbacdef" - Expected output: 6 ("bacdef")
+             * Input "abcdefghi"      - Expected output: 9 ("abcdefghi")
+             * Input "abcdefghd"      - Expected output: 8 ("abcdefgh")
+             * 
+             */
+
+            var word = "abcabcdbb";
+            var result = new StringHelper().GetLongestSequenceLength(word);
+
+            Console.WriteLine($"Longest sequence: {result}");
+        }
+    }
+}
